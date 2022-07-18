d721b9dafef5c4827113ae7d828becc3dbbf7c2e
JackTsai <jacktsai@larvata.tw>
2022-07-18 21:16:01 +0800

    feat: 在 ResourceRecord 增加找出可以建立簽呈資料的單據的查詢方法 load_not_signing。

--- app/models/larvata/signing/resource.rb
+++ app/models/larvata/signing/resource.rb
@@ -1,6 +1,6 @@
 module Larvata::Signing
   class Resource < ApplicationRecord
-    belongs_to :flow, class_name: "Larvata::Signing::Flow", 
+    belongs_to :flow, class_name: "Larvata::Signing::Flow",
       foreign_key: "larvata_signing_flow_id", optional: true
   end
 end
