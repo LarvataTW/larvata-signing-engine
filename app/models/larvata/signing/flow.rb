a9f0b6f0af416f30aca9ce9f7a67dc958732191e
JackTsai <jacktsai@larvata.tw>
2024-07-08 16:32:18 +0800

    fix: 調整 Larvata::Signing::Flow 的  has_many :flow_stages 為 has_many :signing_flow_stages，避免主專案繼承 Larvata::Signing::Flow 之後，操作 accepts_nested_attributes_for 機制會發生錯誤。

--- app/models/larvata/signing/flow.rb
+++ app/models/larvata/signing/flow.rb
@@ -6,15 +6,15 @@ module Larvata::Signing
     has_many :flow_depts, class_name: "Larvata::Signing::FlowDept", foreign_key: "larvata_signing_flow_id"
     has_many :depts, class_name: "Org", through: :flow_depts, source: :dept
 
-    has_many :flow_stages, class_name: "Larvata::Signing::FlowStage",
+    has_many :signing_flow_stages, class_name: "Larvata::Signing::FlowStage",
       foreign_key: "larvata_signing_flow_id"
 
     # 取得指定簽核流程資料結構，並放入簽核單內
     def self.pull_flow(flow_id, current_user)
-      flow = Larvata::Signing::Flow.includes(flow_stages: :flow_members).find_by_id(flow_id)
+      flow = Larvata::Signing::Flow.includes(signing_flow_stages: :flow_members).find_by_id(flow_id)
 
       doc = Larvata::Signing::Doc.new(flow.attributes.slice("remind_period").merge({"larvata_signing_flow_id" => flow_id}))
-      flow.flow_stages.each do |flow_stage|
+      flow.signing_flow_stages.each do |flow_stage|
         stage = doc.stages.build(flow_stage.attributes.slice("typing", "seq"))
 
         unless flow_stage.supervisor_sign?
